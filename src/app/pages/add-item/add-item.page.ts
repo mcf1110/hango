4170ef8d419ae8465d5969e0c9b2297176531a0d
Matheus Fernandes <mcfernandes1110@gmail.com>
2020-09-04 17:41:02 -0300

    fixing add item interface

--- src/app/pages/add-item/add-item.page.ts
+++ src/app/pages/add-item/add-item.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { ItemsService, Item } from 'src/app/services/items.service';
+import { ItemsService } from 'src/app/services/items.service';
+import { PeopleService } from 'src/app/services/people.service';
+import { BehaviorSubject, combineLatest } from 'rxjs';
+import { map, tap, debounceTime, first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-item',
@@ -7,52 +10,58 @@ import { ItemsService, Item } from 'src/app/services/items.service';
   styleUrls: ['add-item.page.scss'],
 })
 export class AddItemPage {
-  constructor(private addItemService: ItemsService) {
+  private search$ = new BehaviorSubject('');
+  private debouncedSearch$ = this.search$.pipe(
+    debounceTime(200),
+    map(s => s.toLocaleLowerCase()),
+  );
+  public people$ = combineLatest([
+    this.debouncedSearch$,
+    this.peopleService.allAlphatically$
+  ]).pipe(
+    map(([s, ps]) => ps.filter(p => p.name.toLocaleLowerCase().includes(s)))
+  );
+  public isSearchEmpty$ = this.debouncedSearch$.pipe(
+    map(s => s.length === 0)
+  );
+
+  public selectedPeople: Set<number> = new Set();
+  public item = {
+    name: '',
+    value: 0
+  }
+
+  constructor(
+    private itemService: ItemsService,
+    private peopleService: PeopleService
+  ) {
 
   }
 
-  public addItem$ = this.addItemService.allAlphatically$;
-  public addItem() {
-    // $state.go('tab.add-item')
+  public searchChanged(search: string) {
+    this.search$.next(search);
   }
-  public editItem(i) {
-    // $state.go('tab.edit-item/{id:int}', {
-    //   id: i
-    // });
+
+  public async checkAll(checked: boolean) {
+    if (!checked) {
+      return this.selectedPeople.clear();
+    }
+    const people = await this.peopleService.allAlphatically$.pipe(first()).toPromise();
+    people.forEach(p => this.selectedPeople.add(p.id));
   }
-  public duplicateItem(i) {
-    // $state.go('tab.add-item/{id:int}', {
-    //   id: i
-    // });
+
+  public toggle(include: boolean, personId: number) {
+    if (include) {
+      return this.selectedPeople.add(personId);
+    }
+    this.selectedPeople.delete(personId);
   }
-  public removeItem(id: number) {
-    this.addItemService.remove(id)
-  }
-  public showSheet(item: Item) {
-    //   var hideSheet = $ionicActionSheet.show({
-    //     buttons: [
-    //       {
-    //         text: 'Editar'
-    //       }, {
-    //         text: 'Duplicar'
-    //       }
-    //     ]
-    //     , destructiveText: 'Remover'
-    //     , titleText: 'Opções'
-    //     , cancelText: 'Cancelar'
-    //     , buttonClicked: function (index) {
-    //       if (index == 0) {
-    //                 public editItem(id);
-    //   }
-    //             if (index == 1) {
-    //                 public duplicateItem(id);
-    //   }
-    //   return true;
-    // }
-    //         , destructiveButtonClicked: function () {
-    //             public removeItem(id);
-    //   return true;
-    // }
-    // });
+
+  public submit() {
+    const people = [...this.selectedPeople]
+    console.log({
+      ...this.item,
+      people
+    });
   }
 }
